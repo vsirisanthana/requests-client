959f38911931dc44f2198b2ee92caa2763deacc8
vsirisanthana <vsirisanthana@gmail.com>
2012-02-27 15:16:13 +0000

    Add different urls, queries tests

--- requests_wrapper/tests/test_client.py
+++ requests_wrapper/tests/test_client.py
@@ -17,18 +17,60 @@ class TestClient(TestCase):
     def test_get_cache_control(self, mock_get):
         response = Response()
         response.status_code = 200
-        response._content = 'Test mock'
+        response._content = 'Mocked response content'
         response.headers = {
             'Cache-Control': 'max-age=1',
         }
         mock_get.return_value = response
 
-        client.get('http://test')
+        client.get('http://www.test.com/path')
         self.assertEqual(mock_get.call_count, 1)
-        client.get('http://test')
+        client.get('http://www.test.com/path')
         self.assertEqual(mock_get.call_count, 1)
 
         sleep(1)
 
-        client.get('http://test')
+        client.get('http://www.test.com/path')
         self.assertEqual(mock_get.call_count, 2)
+
+    @patch('requests.get')
+    def test_get_different_urls(self, mock_get):
+        response = Response()
+        response.status_code = 200
+        response._content = 'Mocked response content'
+        response.headers = {
+            'Cache-Control': 'max-age=10',
+            }
+        mock_get.return_value = response
+
+        client.get('http://www.test.com/path/1')
+        self.assertEqual(mock_get.call_count, 1)
+        client.get('http://www.test.com/path/2')
+        self.assertEqual(mock_get.call_count, 2)
+        client.get('http://www.test.com/path/1')
+        self.assertEqual(mock_get.call_count, 2)
+        client.get('http://www.test.com/path/3')
+        self.assertEqual(mock_get.call_count, 3)
+        client.get('http://www.test.com/path/2')
+        self.assertEqual(mock_get.call_count, 3)
+
+    @patch('requests.get')
+    def test_get_different_queries(self, mock_get):
+        response = Response()
+        response.status_code = 200
+        response._content = 'Mocked response content'
+        response.headers = {
+            'Cache-Control': 'max-age=10',
+            }
+        mock_get.return_value = response
+
+        client.get('http://www.test.com/path?name=john')
+        self.assertEqual(mock_get.call_count, 1)
+        client.get('http://www.test.com/path?name=emily')
+        self.assertEqual(mock_get.call_count, 2)
+        client.get('http://www.test.com/path?name=john&age=30')
+        self.assertEqual(mock_get.call_count, 3)
+        client.get('http://www.test.com/path?name=emily')
+        self.assertEqual(mock_get.call_count, 3)
+        client.get('http://www.test.com/path?name=john&age=30')
+        self.assertEqual(mock_get.call_count, 3)
