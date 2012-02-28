42926e942e20588c5d70878dba0ca2af4d5c689d
cuppiecake <cuppiecake@gmail.com>
2012-02-28 10:24:54 +0000

    add cache permanent redirect 301 status code

--- requests_wrapper/client.py
+++ requests_wrapper/client.py
@@ -3,8 +3,15 @@ from requests import *
 from django.http import HttpRequest, HttpResponse
 from requests_wrapper.cache import CacheManager
 
+from django.core.cache import cache
 
 def get(url, **kwargs):
+
+    # check if the url is permanently redirect or not
+    redirect_to = cache.get('redirect.%s' % url)
+    if redirect_to:
+        return get(redirect_to, **kwargs)
+
 
     # Construct Django HttpRequest object
     http_request = HttpRequest()
@@ -22,10 +29,17 @@ def get(url, **kwargs):
     if response:
         return response
 
+    kwargs['allow_redirects'] = False
     response = requests.get(url, **kwargs)
 
+    if response.status_code == 301:
+        #TODO: handle case of no Location header
+        redirect_to = response.headers.get('Location')
+        cache.set('redirect.%s' % url, redirect_to )
+        return get(redirect_to, **kwargs)
+
     # TODO:
-    # 1. Check for 301
+    # 1. Check for 301 -- DONE!!!
 #    if response.status_code == 301:
 #        cache.set('redirect.%s' % url, )
 
