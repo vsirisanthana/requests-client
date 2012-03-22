b8878a387a5c8fa1f97f694008c56fbc80b50d6a
cuppiecake@gmail.com <cuppiecake@gmail.com@d2b86d2c-6f83-31c9-230b-0118b336ca6c>
2012-03-22 19:24:28 +0000

    add test cookie

--- branches/DOG5_20120319_euam_clear_expired_cookie/requests_wrapper/cookie.py
+++ branches/DOG5_20120319_euam_clear_expired_cookie/requests_wrapper/cookie.py
@@ -1,71 +1,63 @@
-from Cookie import SimpleCookie, _getdate
-from datetime import datetime, timedelta
+from Cookie import SimpleCookie
+from datetime import datetime
 from urlparse import urlparse
 from django.core.cache import cache
-from time import gmtime, mktime, strptime
 
 def get_domain_cookie(url):
+    """
+    return simple dictionary of (key:value) cookies for domain
+    """
     domain = urlparse(url).netloc
-#    cookies = cache.get('cookies')
     set_cookie = {}
-#    if cookies and cookies.get(domain):
+    expired_cookie = set()
     if cache.get(domain):
-#        domain_cookie = cookies.get(domain)
-        domain_cookie = cache.get(domain)
-#        for name, cookie in domain_cookie.items():
-#            #check only un expired cookie can get in the dict
-#            if cookie['expires']:
-#                fmt = '%a, %d-%b-%Y %H:%M:%S GMT'
-#                # otherwise have to convert back to epoh seconds :( make it gmt time when compare
-#                expired  = gmtime(mktime(strptime(cookie['expires'],fmt)))
-#                now = gmtime(mktime(strptime(_getdate(),fmt)))
-#                if now > expired:
-#                    del domain_cookie[name]
-#                    continue
-#            set_cookie.update({name:cookie.value})
-
-        for cookiename in domain_cookie:
-            cookie = cache.get(cookiename)
+        domain_cookies = cache.get(domain)
+        for cookie_name in domain_cookies:
+            cookie = cache.get(cookie_name)
             if cookie:
                 set_cookie.update({cookie.key: cookie.value})
+            else:
+                expired_cookie.add(cookie_name)
+
+        updated_domain_cookies = domain_cookies.difference(expired_cookie)
+        cache.set(domain, updated_domain_cookies)
 
     return  set_cookie
 
 
 def extract_cookie(url, response):
-#    cookies = cache.get('cookies')
-    if response.cookies:
-#        cookies = cookies or dict()
+    #if there's set-cookie in response
+    if response and response.cookies:
         domain = urlparse(url).netloc
-#        if not domain in cookies.keys():
+        #if there's not cookie for this domain, create one
         if not cache.get(domain):
-            #cookies[domain] = {}
             cache.set(domain,set())
-
-#        domain_cookies = cookies[domain]
         domain_cookies = cache.get(domain)
         cookie = SimpleCookie()
         cookie.load(response.headers['set-cookie'])
         for name, c in cookie.items():
-            cookiename = '%s.%s' % (domain, name)
-            maxage = None
+            cookie_name = '%s.%s' % (domain, name)
+            max_age = None
+
+            # convert expires to seconds, so we use take advantage of cache expiry feature,
+            # no need to clear cookie ourselves
             if c['expires']:
                 fmt = '%a, %d-%b-%Y %H:%M:%S GMT'
                 expired  = datetime.strptime(c['expires'], fmt)
                 now = datetime.utcnow()
-                maxage = (expired - now).total_seconds()
+                max_age = (expired - now).total_seconds()
 
+            # max-age has higher priority than  expires
             if c['max-age']:
-#                c['expires'] = _getdate(future=int(c['max-age']))
-                maxage = int(c['max-age'])
-            if maxage is not None:
-                cache.set(cookiename, c, maxage)
+                max_age = int(c['max-age'])
+
+            # save morsel (cookie info) in cache :)
+            if max_age is not None:
+                cache.set(cookie_name, c, max_age)
             else:
-                cache.set(cookiename, c)
+                cache.set(cookie_name, c)
 
-            domain_cookies.add(cookiename)
+            domain_cookies.add(cookie_name)
 
-#        domain_cookies.update(cookie)
-#        cache.set('cookies', cookies)
         cache.set(domain, domain_cookies)
 
