2bc34a8e469afe439fcb1b5b93e671355a8292cc
vsirisanthana <vsirisanthana@gmail.com>
2012-03-27 11:03:40 +0000

    Rename dogutils to utils

--- cache.py
+++ cache.py
@@ -1,4 +1,4 @@
-from dogutils.cache import get_cache_key, learn_cache_key, get_max_age
+from utils.cache import get_cache_key, learn_cache_key, get_max_age
 
 
 CACHE_MANAGER_LONG_TERM_CACHE_KEY_PREFIX = 'longterm'
