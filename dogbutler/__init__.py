974d1568768d7d9950c9885dd4f538b65d9f5bf5
vsirisanthana <vsirisanthana@gmail.com>
2012-03-31 16:47:21 +0000

    setup.py script

--- dogbutler/__init__.py
+++ dogbutler/__init__.py
@@ -1,1 +1,24 @@
-__author__ = 'pique'
+# -*- coding: utf-8 -*-
+
+#   __          __
+#  /  ) _   _  /__)   _/ / _  _
+# /__/ (_) (/ /__) (/ / / (- /
+#         _/
+
+"""
+DogButler
+~~~~~~~~
+
+:copyright: (c) 2012 by Vichaya/Euam Sirisanthana.
+:license: ISC, see LICENSE for more details.
+
+"""
+
+__title__ = 'requests'
+__version__ = '0.0.1'
+__build__ = 0x000001
+__author__ = 'Vichaya/Euam Sirisanthana'
+__license__ = 'ISC'
+__copyright__ = 'Copyright 2012 Vichaya/Euam Sirisanthana'
+
+
